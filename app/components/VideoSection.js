37c95a14ff7f0f5500fbe3927e1df6544a46d7c3
saquelain <saquelain1502@gmail.com>
2026-06-08 22:45:31 +0530

    youtube video source url changed

--- app/components/VideoSection.js
+++ app/components/VideoSection.js
@@ -6,8 +6,8 @@ export default function VideoSection() {
         <h2 className="section-title">Experience Our Campus</h2>
         <div className="video-wrapper">
           <iframe
-            src="https://www.youtube.com/embed/dthc3tAB08U"
-            title="Mata Gujri Memorial Medical College, Kishanganj, Aerial &amp; Inner View"
+            src="https://www.youtube.com/embed/npQtBi-G4Dk"
+            title="Mata Gujri University, Kishanganj, Bihar- Aerial View !!"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerPolicy="strict-origin-when-cross-origin"
             allowFullScreen
