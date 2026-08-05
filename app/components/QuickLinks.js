e5dd6ad6ed75a33bafbdf207521c1b62925310ee
saquelain mokhtar <saquelain1502@gmail.com>
2026-08-05 11:08:00 +0530

    quicklinks added for admission button

--- app/components/QuickLinks.js
+++ app/components/QuickLinks.js
@@ -5,7 +5,7 @@ export default function QuickLinks() {
   return (
     <div className="quick-links">
       <div className="quick-links-inner">
-        <a href="#" className="quick-link">
+        <a href="/admissions" className="quick-link">
           <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M22 10v6M2 10l10-5 10 5-10 5z"/><path d="M6 12v5c3 3 9 3 12 0v-5"/></svg>
           Admissions 2026-27
         </a>
